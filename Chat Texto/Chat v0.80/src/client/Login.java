25b28a3cfe343457dc24f6c065d25ba7b23efa02
DavKiin <davipontes38@hotmail.com>
2023-05-23 11:40:52 -0300

    Chat v0.80 - (Work in Progress)
    Trabalho em progresso.

---
+++ Chat Texto/Chat v0.80/src/client/Login.java
@@ -0,0 +1,116 @@
+package client;
+
+import common.ArchiveUtils;
+import common.InitHUD;
+import common.Utils;
+import server.Server;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ThreadLocalRandom;
+import java.awt.*;
+import javax.swing.*;
+
+public class Login extends InitHUD {
+
+    private JButton jbLoogin; // botao de login
+    private JLabel jlUser, jlPort, title; // labels visuais
+    private JTextField jtUser, jtPort; // zona de texto com infos
+    
+    public Login() {
+        super("Login");
+    }
+
+    protected void initComponents() {
+        title = new JLabel("CHAT", SwingConstants.CENTER);
+        jbLoogin = new JButton("Entrar");
+        jlUser = new JLabel("Apelido", SwingConstants.CENTER);
+        jlPort = new JLabel("Porta", SwingConstants.CENTER);
+        jtUser = new JTextField();
+        jtPort = new JTextField();
+    }
+
+    @Override
+    protected void configComponents() {
+        this.setLayout(null); 
+        this.setMinimumSize(new Dimension(400, 450));
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(Color.WHITE);
+        
+            //titulo grafico exibido em tela
+        title.setBounds(15,10,350,200);
+        ImageIcon logo = new ImageIcon("../assets/Logo_Chat.png");
+        title.setIcon(new ImageIcon(logo.getImage().getScaledInstance(375, 100, Image.SCALE_SMOOTH)));
+            // Botão de login exibido em tela
+        jbLoogin.setBounds(15,320,350,40);
+            //Botão e caixa de usuario
+        jlUser.setBounds(15,220,100,40);
+        jlUser.setBorder(BorderFactory.createLineBorder(Color.GRAY));   
+        jtUser.setBounds(120,220,250,40);
+    }
+
+    @Override
+    protected void insertComponents() {
+        this.add(title);
+        this.add(jbLoogin);
+        this.add(jlPort);
+        this.add(jlUser);
+        this.add(jtPort);
+        this.add(jtUser);
+    }
+
+    
+    @Override
+    protected void insertActions() {
+            //Tela com update
+                String read = ArchiveUtils.ReadArchive("D:/Projetos/Projetos Academico/Chat Texto/Chat v0.80/lib/update/Update.txt");
+                JOptionPane.showMessageDialog(this, read);
+            //Inserindo ação de login pos pressionamento do botão
+        jbLoogin.addActionListener(event -> {
+            Socket coneS;
+            //Gerador Randomico de porta client
+            int randomPort = ThreadLocalRandom.current().nextInt(Server.PORT, 65535) - Server.PORT; //randomgenerator
+            final String random = Integer.toString(randomPort); //String para JTport
+            //Definição de porta ao campo
+            jtPort.setText(random);
+            System.out.println("[CLIENT:LOGIN.jtPort] Porta Randomica Cliente iniciada " + random);
+
+            try {
+                String nick = jtUser.getText(); // nickname do usuario
+                int port = Integer.parseInt(jtPort.getText()); // porta pertencente ao usuario
+                
+                jtUser.setText("");
+                
+
+                    //iniciando socket client
+                coneS = new Socket(Server.HOST, Server.PORT);
+                String conecInfo = nick + ":" + coneS.getLocalAddress().getHostAddress() + ":" + port;
+                Utils.enviaMensagens(coneS, conecInfo); // envia a Utils socket e informações de conexao
+                if (Utils.mensagemRecebida(coneS).toLowerCase().equals("sucess")) {
+                    new Home(coneS, conecInfo); // cria nova tela de home
+                    this.dispose();
+                } else {
+                    JOptionPane.showMessageDialog(this, "Porta Utilizada"); // caso o nick ou porta esteja utilizado
+                }
+            } catch (IOException ex) {
+                System.err.println("[ERROR:LOGIN.IOException]: " + ex.getMessage());
+                JOptionPane.showMessageDialog(this, "Erro ao conectar ao servidor.");
+            }
+            
+        });
+    }
+
+
+    @Override
+    protected void start() {
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        
+        Login login = new Login();
+    
+    }
+
+}
---
+++ Chat Texto/Chat v0.80/src/common/ArchiveUtils.java
@@ -0,0 +1,64 @@
+package common;
+
+import java.io.*;
+
+public class ArchiveUtils{
+    public String diretório;
+
+        //Leitor do arquivo
+    public static String ReadArchive (String directory){
+        String content = ""; //conteudo declarado null
+
+        try {
+            FileReader arch = new FileReader(directory); //leitor do diretório
+            BufferedReader read = new BufferedReader(arch); //buffer de leitura do arquivo
+            String line = ""; //linha declarada null;
+            
+            try {
+                line = read.readLine();
+                    //Enquanto ouver linha ele acrescentará novas linhas sob o conteúdo.
+                while (line != null) {
+                    content += line + "\n";
+                    line = read.readLine();
+                } 
+                arch.close();
+
+            } catch (IOException ex) {
+                    //Leitura de arquivo.
+                System.err.println("[ERROR:commom.ArchiveUtils.readArchive]: Não foi possível ler o arquivo");
+                return "";
+            }
+        } catch (FileNotFoundException ex) {
+                //Disponibilidade de arquivo.
+            System.err.println("[ERROR:commom.ArchiveUtils.readArchive]: Arquivo não encontrado no diretório" + directory);
+            return "";
+        } 
+            //Caso conteúdo expresso tenha ERRO retorno esperado. 
+        if(content.contains("Erro")){
+            return "";
+        } else {
+            return content;
+        }
+    }
+
+        //Leitura de arquivo
+    public static boolean WriteArchive(String directory, String text) {
+        
+        try {
+            FileWriter arch = new FileWriter(directory); // leitor do diretorio 
+            PrintWriter printArch = new PrintWriter(arch); // metodo de escrita
+                //Print dos textos em um arquivo
+            printArch.println(text); // Escrita no arquivo.
+            printArch.close();
+
+            return true;
+        } catch (IOException ex) {
+                //Em casos de problemas com leitura.
+            System.err.println("[ERROR:commom.ArchiveUtils.WriteArchive]:" + ex.getMessage());
+            return false;
+        }
+    }
+   
+}
+    
+    
